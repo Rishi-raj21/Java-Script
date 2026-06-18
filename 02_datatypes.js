f05d819e29cafd519b872aa2bc3038a89fe12da2
Rajat Dhar Dwivedi <rajatdd2004@gmail.com>
2026-06-18 10:40:21 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) we are using node js not browser
+
+let name="Rajat"    //string
+let age=18          //numbers
+let isLoggedIn=false    //boolean
+// null-> standalone value
+// undefined -> 
+// symbol-> umiqueness
+
+console.log(typeof "Rajat")
+console.log(typeof 3)
+console.log(typeof null)
